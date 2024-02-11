d3edc31b4a2beb292d54494724fde4278406abab
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2024-02-11 11:59:22 -0400

    first step

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import logo from "./assets/logo-nlw.svg";
+//@ts-ignore
+import { NewNoteCard } from './components/newnotecard';
+//@ts-ignore
+import { NoteCard } from './components/notecard';
+
+
 export function App() {
- return <h1>hello world</h1>
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="NLW Expert" />
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard/>
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+
+        
+      </div>
+    </div>
+  );
 }
-
-
