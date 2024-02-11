6b1c9829d36a69b307ba9cb5111661824bb718d7
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2024-02-11 15:48:21 -0400

    3 step, nlw finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,61 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo-nlw.svg";
 import { NewNoteCard } from './components/newnotecard';
 import { NoteCard } from './components/notecard';
 
+
+
+
+interface Note{
+  id: string
+  date: Date
+  content : string
+}
+
 export function App() {
-  const [notes, setNotes] = useState({
+  const [search ,setSearch] = useState("")
+  const [notes, setNotes] = useState<Note[]>(()=>{
     
+    const noteOnStorage = localStorage.getItem('notes')
+
+    if (noteOnStorage){
+      return JSON.parse(noteOnStorage)
+    }
+    return []
   })
+
+function onNoteCreated(content:string){
+  const newNote = {
+    id:crypto.randomUUID(),
+    date: new Date(),
+    content,
+  }
+  const noteArrays = [ newNote, ...notes]
+  setNotes(noteArrays)
+  
+  localStorage.setItem('notes', JSON.stringify(noteArrays))
+}
+
+function handleSearch(event: ChangeEvent<HTMLInputElement>){
+  const query = event.target.value
+
+  setSearch(query)
+  
+}
+
+function onNoteDelete(id:string){
+   const notesArray = notes.filter(note=>{
+    return note.id != id
+   })
+
+   setNotes(notesArray)
+   localStorage.setItem('notes', JSON.stringify(notesArray))
+}
+
+const filteredNotes = search!="" ? notes.filter(note => note.content.toLocaleLowerCase( ).includes(search.toLocaleLowerCase())) : notes;
+
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NLW Expert" />
 
       <form className="w-full">
@@ -16,14 +63,17 @@ export function App() {
           type="text"
           placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+          onChange={handleSearch}
         />
       </form>
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard/>
-        <NoteCard note={{date:new Date(), content: "hello world"}}/>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+      {filteredNotes.map(note =>{
+          return <NoteCard  key={note.id} note={note} onNoteDelete={onNoteDelete}/>
+      })}
        
 
         
